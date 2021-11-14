e154d384f96a85a4f3b9e660ce95b5395d4e945f
diogomarv <diogomarv@gmail.com>
2021-11-14 09:59:04 -0300

    Added EntityFramework and context

---
+++ Back-end/InventoryZ.API/InventoryZ.Infrastructure/Persistence/DataBaseContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryZ.Infrastructure.Persistence
+{
+    public class DataBaseContext : DbContext
+    {
+        public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options) { }
+
+
+    }
+}
