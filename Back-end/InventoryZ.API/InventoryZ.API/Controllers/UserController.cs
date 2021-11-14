3e63bf7b90a691882e7e51ba702c891a3c35706a
diogomarv <diogomarv@gmail.com>
2021-11-14 08:51:04 -0300

    UserController created

---
+++ Back-end/InventoryZ.API/InventoryZ.API/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InventoryZ.API.Controllers
+{
+    [Route("/api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        [HttpGet("example")]
+        public IActionResult JustTest()
+        {
+            return Ok();
+        }
+    }
+}
