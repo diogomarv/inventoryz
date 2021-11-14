efbfe574837b83c77cfe437219cdf58fcb906658
diogomarv <diogomarv@gmail.com>
2021-11-14 17:45:16 -0300

    Added Product status Enum

--- Back-end/InventoryZ.API/InventoryZ.Core/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace InventoryZ.Core
-{
-    public class Class1
-    {
-    }
-}
---
+++ Back-end/InventoryZ.API/InventoryZ.Core/Enums/ProductStatusEnum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryZ.Core.Enums
+{
+    public enum ProductStatusEnum
+    {
+        Availiable = 0,
+        Unavailable = 1
+    }
+}
